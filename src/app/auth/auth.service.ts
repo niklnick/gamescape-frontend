8b19c7ad69c7792c0e90306f6b652f548825ec74
neumann.niklas <neumann.niklas98@gmail.com>
2024-10-17 20:04:03 +0200

    environments

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 import { Auth } from './models/auth.model';
 import { LogIn } from './models/log-in.model';
 import { SignUp } from './models/sign-up.model';
@@ -9,19 +10,19 @@ import { SignUp } from './models/sign-up.model';
   providedIn: 'root'
 })
 export class AuthService {
-  private readonly API_URL: string = 'http://localhost:3000/auth/';
+  private readonly API_URL: string = environment.apiUrl + '/auth';
 
   constructor(private readonly httpClient: HttpClient) { }
 
   signUp(signUp: SignUp): Observable<Auth> {
-    return this.httpClient.post<Auth>(this.API_URL + 'sign-up', signUp);
+    return this.httpClient.post<Auth>(this.API_URL + '/sign-up', signUp);
   }
 
   logIn(logIn: LogIn): Observable<Auth> {
-    return this.httpClient.post<Auth>(this.API_URL + 'log-in', logIn);
+    return this.httpClient.post<Auth>(this.API_URL + '/log-in', logIn);
   }
 
   refresh(): Observable<Auth> {
-    return this.httpClient.get<Auth>(this.API_URL + 'refresh');
+    return this.httpClient.get<Auth>(this.API_URL + '/refresh');
   }
 }
